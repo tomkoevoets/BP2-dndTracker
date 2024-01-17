de9bec3647f1b5f47f06faf4f97cc4afa42df870
tomkoevoets <tomkoevoets1@gmail.com>
2024-01-17 11:43:45 +0100

    added main controller functions and generateSessionItem
    added main controller functions:
       - get
       - create
       - update
       - delete
    
    added sessionItem method to SessionScreen:
       - generate session picture and
         display
       - generate session name and display

--- bp2-dnd-tracker/src/main/java/com/dndtracker/bp2dndtracker/classes/Controller.java
+++ bp2-dnd-tracker/src/main/java/com/dndtracker/bp2dndtracker/classes/Controller.java
@@ -1,4 +1,108 @@
 package com.dndtracker.bp2dndtracker.classes;
 
+import java.util.ArrayList;
+
 public class Controller {
+
+    private Database db;
+
+    public Controller() {
+        this.db = new Database();
+    }
+///////////////////////////////////////////////Campaign/////////////////////////////////////////////////////////////////
+    //TODO Create GetAllCampaigns method
+
+    //TODO Create CreateCampaign method
+
+
+    //TODO Create DeleteCampaign method
+
+
+    //TODO Create UpdateCampaign method
+
+
+///////////////////////////////////////////////Session//////////////////////////////////////////////////////////////////
+    //TODO Create GetAllCharacter method
+    public ArrayList<Session> getSessions() {
+        return db.getAllSessions();
+    }
+
+    //TODO Create CreateCharacter method
+    public void createSession(String name, String info) {
+        db.createSession(name, info);
+    }
+
+    //TODO Create DeleteCharacter method
+    public void deleteSession(int id) {
+        db.deleteSession(id);
+    }
+
+    //TODO Create UpdateCharacter method
+    public void updateSession(Session session) {
+        db.updateSession(session);
+    }
+
+///////////////////////////////////////////////Character////////////////////////////////////////////////////////////////
+    //TODO Create GetAllCharacter method
+
+
+    //TODO Create CreateCharacter method
+    public void createCharacter(String type, String name, String description, String image, String extra, int armorClass, String hitPoints,
+                                String strength, String dexterity, String constitution, String intelligence, String wisdom,
+                                String charisma, String speed, int challenge, String sense, String languages, String skills) {
+        db.createCharacter(type, name, description, image, extra, armorClass, hitPoints, strength, dexterity, constitution, intelligence, wisdom, charisma, speed, challenge, sense, languages, skills);
+    }
+
+
+
+    //TODO Create DeleteCharacter method
+    public void deleteCharacter(int id) {
+        db.deleteCharacter(id);
+    }
+
+
+    //TODO Create UpdateCharacter method
+    public void updateCharacter(CharacterSuperclass character) {
+        db.updateCharacter(character);
+    }
+
+
+///////////////////////////////////////////////Monster-specific/////////////////////////////////////////////////////////
+    //TODO Create GetAllMonsters method
+    public ArrayList<CharacterSuperclass> getMonsters() {
+        return db.getAllCharacters("Monster");
+    }
+
+
+///////////////////////////////////////////////Npc-specific/////////////////////////////////////////////////////////////
+    //TODO Create GetAllNpc's method
+    public ArrayList<CharacterSuperclass> getNpcs() {
+        return db.getAllCharacters("Npc");
+    }
+
+
+///////////////////////////////////////////////Item/////////////////////////////////////////////////////////////////////
+    //TODO Create GetAllItems method
+    public ArrayList<Item> getItems() {
+        return db.getAllItems();
+    }
+
+    //TODO Create CreateItem method
+    public void createItem(String name, String description, String image, String type, String rarity, int cost, double weight, String extra) {
+        db.createItem(name, description, image, type, rarity, cost, weight, extra);
+    }
+
+
+    //TODO Create DeleteItem method
+    public void deleteItem(int id) {
+        db.deleteItem(id);
+    }
+
+
+    //TODO Create UpdateItem method
+    public void updateItem(Item item) {
+        db.updateItem(item);
+    }
+
 }
+
--- bp2-dnd-tracker/src/main/java/com/dndtracker/bp2dndtracker/screens/SessionScreen.java
+++ bp2-dnd-tracker/src/main/java/com/dndtracker/bp2dndtracker/screens/SessionScreen.java
@@ -1,6 +1,8 @@
 package com.dndtracker.bp2dndtracker.screens;
 
 import com.dndtracker.bp2dndtracker.Application;
+import com.dndtracker.bp2dndtracker.classes.Database;
+import com.dndtracker.bp2dndtracker.classes.Session;
 import com.dndtracker.bp2dndtracker.components.BackgroundComponent;
 import com.dndtracker.bp2dndtracker.components.SidebarComponent;
 import javafx.geometry.Insets;
@@ -9,9 +11,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.OverrunStyle;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
 
 public class SessionScreen  {
 
@@ -20,6 +23,8 @@ public class SessionScreen  {
     private final BackgroundComponent background;
 
     public SessionScreen() {
+        Database db = new Database();
+
         HBox root = new HBox();
         scene = new Scene(root, 1400, 750);
         scene.getStylesheets().add(Application.class.getResource("stylesheets/sidebar.css").toString());
@@ -36,9 +41,68 @@ public class SessionScreen  {
 //        add the background from components
         background = new BackgroundComponent();
 
+        content.getChildren().add(background);
 
-        content.getChildren().addAll(background);
+        for (Session session : db.getAllSessions()) {
+            content.getChildren().add(generateSessionItem(session));
+        }
+
         root.getChildren().addAll(sidebar, content);
+    }
+
+    public HBox generateSessionItem(Session session) {
+        //TODO make session items appear structured
+        //TODO create black background with gold border?
+        HBox sessionItem = new HBox();
+        sessionItem.setAlignment(Pos.CENTER);
+        sessionItem.setMinSize(130, 232);
+//        sessionItem.setMaxSize(130, 232);
+//        sessionItem.setSpacing(50);
+        sessionItem.setId("film-item");
+        sessionItem.setStyle("-fx-background-color: green");
+
+        FlowPane sessionPicture = new FlowPane(Orientation.VERTICAL);
+        sessionPicture.setPrefSize(120, 175);
+        sessionPicture.setMinSize(120, 175);
+        sessionPicture.setMaxSize(120, 175);
+        sessionPicture.setId("session-picture");
+        sessionPicture.setAlignment(Pos.CENTER);
+        sessionPicture.setStyle("-fx-background-color: blue");
+//        sessionPicture.setStyle("-fx-border-radius: 10px; -fx-background-radius: 10px;");
+
+        // rectangle to clip the picture to the size of the session item and give it a radius
+        //TODO get clipreact to be centered over the image
+        Rectangle clipRect = new Rectangle(120, 175);
+        clipRect.setArcWidth(10);
+        clipRect.setArcHeight(10);
+        sessionPicture.setClip(clipRect);
+
+        ImageView sessionPictureImage = new ImageView();
+//        sessionPictureImage.setPreserveRatio(true);
+        sessionPictureImage.setSmooth(true);
+        sessionPictureImage.setFitWidth(120);
+        sessionPictureImage.setFitHeight(175);
+        sessionPictureImage.setImage(new Image(Application.class.getResource("images/session-item-alt-pic.jpg").toString()));
+        sessionPictureImage.setId("session-picture-image");
+
+        sessionPictureImage.setStyle(
+                        "-fx-background-size: cover;" +
+                        "-fx-background-repeat: no-repeat;" +
+                        "-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.2), 20, 0, 0.0, 0.0);" +
+                        "-fx-background-radius: 10px;"
+        );
+
+        //TODO create white text for the session title
+        Label sessionTitle = new Label(session.getName());
+        sessionTitle.setWrapText(true);
+//        sessionTitle.setAlignment(Pos.TOP_LEFT);
+        sessionTitle.setId("session-title");
+
+        sessionPicture.getChildren().add(sessionPictureImage);
+        sessionItem.getChildren().addAll(sessionPicture, sessionTitle);
+
+
+        return sessionItem;
     }
 
     public Scene getScene() {
